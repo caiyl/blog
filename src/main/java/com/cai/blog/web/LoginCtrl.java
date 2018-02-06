a5717d676fc9d9b7ef8aaa34b160457bae3caee9
caiyl <493199904@qq.com>
2018-02-06 10:27:53 +0800

    sss

--- src/main/java/com/cai/blog/web/LoginCtrl.java
+++ src/main/java/com/cai/blog/web/LoginCtrl.java
@@ -1,5 +1,6 @@
 package com.cai.blog.web;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,4 +16,17 @@ public class LoginCtrl {
     public String hello(){
         return "Hello! login!";
     }
+
+
+    @RequestMapping("/jscode2session/{code}")
+    @ResponseBody
+    public String jscode2session(@PathVariable String code){
+        System.out.println("code:"+code);
+        String url = "https://api.weixin.qq.com/sns/jscode2session?appid=wx442fbd38e3a66a7f&secret=2cd91ee9b11cbaaf70611c32aff7cbc8&js_code="+code+"&grant_type=authorization_code";
+
+
+
+        return "Hello! login!";
+    }
+
 }
